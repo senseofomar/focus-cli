0995259a545c677398814da67eb94132f7a0dd30
senseofomar <senseofomar@gmail.com>
2025-09-16 17:00:51 +0530

    rev 1

--- practice/task_manager.py
+++ practice/task_manager.py
@@ -1,6 +1,21 @@
+import csv
+
+
 class TaskManager:
     def __init__(self, filepath)->None:
         self.filepath = filepath,
-        self.task = []
-        self.load_task()
+        self.tasks = []
+        self.load_tasks()
 
+    def add_task(self, task)->None:
+        self.tasks.append(task)
+        self.save_tasks()
+
+    def save_tasks(self)->None:
+        with open(self.filepath, mode = "w", newline="", encoding="utf-8") as f:
+            writer = csv.DictWriter(f, fieldnames=[
+                'Title', 'Priority', 'Deadline',
+                'Category', 'Completed'])
+            writer.writeheader() #Writes the first row of the CSV — the column headers (Title, Due Date, Priority, Category, Completed).
+            for task in self.tasks:
+                writer.writerow(task.to_dict()) #Writes one row to the CSV for each task.
