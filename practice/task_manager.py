992158ab0470b40818eed652000dd68bb14a6bb5
senseofomar <senseofomar@gmail.com>
2025-09-16 17:30:23 +0530

    rev 1

--- practice/task_manager.py
+++ practice/task_manager.py
@@ -1,9 +1,11 @@
 import csv
+import os
+from practice.task import Task
 
 
 class TaskManager:
     def __init__(self, filepath)->None:
-        self.filepath = filepath,
+        self.filepath = filepath
         self.tasks = []
         self.load_tasks()
 
@@ -19,3 +21,29 @@ class TaskManager:
             writer.writeheader() #Writes the first row of the CSV — the column headers (Title, Due Date, Priority, Category, Completed).
             for task in self.tasks:
                 writer.writerow(task.to_dict()) #Writes one row to the CSV for each task.
+
+
+    def load_tasks(self)->None:
+        if not os.path.exists(self.filepath):
+            return
+        with open(self.filepath, mode ='r',newline ='' ,encoding ='utf-8') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                self.tasks.append(Task.from_dict(row))
+
+
+    def list_tasks(self, show_completed = True)->None:
+        for index, task in enumerate(self.tasks, start =1):
+            if not show_completed and task.completed:
+                continue
+            status = "✅" if task.completed else "❌"
+            print(
+                f"{index}. {task.tt}| Deadline: {task.dd} | Priority: {task.prt} | Category: {task.cat} | Completed: {status}")
+
+
+    def complete_task(self, index)-> None:
+        if 0<= index<len(self.tasks):
+            self.tasks[index].mark_complete()
+            self.save_tasks()
+        else:
+            print("❗ Invalid task number.")
