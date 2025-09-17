564ae1fda71c3798fe05a2c296d4aec23b09df2a
senseofomar <senseofomar@gmail.com>
2025-09-17 09:39:48 +0530

    rev day 2

--- src/task_manager.py
+++ src/task_manager.py
@@ -10,7 +10,7 @@ class TaskManager:
         self.tasks = []
         self.load_tasks()
 
-    def add_tasks(self)-> None:
+    def add_tasks(self, task)-> None:
         self.tasks.append(task)
         self.save_tasks()
 
@@ -29,4 +29,25 @@ class TaskManager:
         with open (self.filepath, mode ='r', newline = '', encoding = 'utf-8') as f:
             reader = csv.DictReader(f)
             for row in reader:
-                self.tasks.append(Task.from_dict())
+                try:
+                    self.tasks.append(Task.from_dict(row))
+                except KeyError as e:
+                    print(f"⚠️ Skipping row due to missing column: {e}")
+
+    def list_tasks(self, show_completed =True)->None:
+        for idx, task in enumerate( self.tasks, start = 1):
+            if not show_completed and task.completed:
+                continue
+            status = "✅" if task.completed else "❌"
+            print(
+                f"{idx}. {task.title}| Deadline: {task.due_date} | Priority: {task.priority} | Category: {task.category} | Completed: {status}")
+
+
+    def complete_task(self, idx)->None:
+        if 0<= idx< len(self.tasks):
+            self.tasks[idx].mark_complete()
+            self.save_tasks()
+        else:
+            print("❗ Invalid task number.")
+
+
